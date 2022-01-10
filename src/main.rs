7bbc006be6dcf0e0385765807adf161cbb9255f7
libvirgo <sakurapetgirl@live.com>
2022-01-10 18:15:14 +0800

    [feature] hello world system.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
 mod lang_item;
 mod sbi;
+mod console;
 
 use core::arch::global_asm;
 
@@ -11,7 +13,8 @@ global_asm!(include_str!("entry.asm"));
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    loop {}
+    println!("Hello, world!");
+    panic!("Shutdown machine");
 }
 
 fn clear_bss() {
